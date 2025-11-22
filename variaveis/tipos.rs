1bd88c5a4f10380849a946900e2676c4174ecd1c
Josemarcio15 <josemarcio0araujo@gmail.com>
2025-11-22 02:51:17 -0400

    FirstCommit

---
+++ variaveis/tipos.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let funcao = get_nome();
+    let funcao2 = imprimir();
+
+    print!("meu nome é {}{}", funcao, funcao2);
+}
+
+fn get_nome() -> String {
+    String::from("marcio")
+}
+
+fn imprimir() -> {
+    print!("Hola")
+}
